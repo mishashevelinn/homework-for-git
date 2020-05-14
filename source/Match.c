581ac632bf63c8e44f46ca4e2fa07d89c32cd921
Talsha1995 <64966800+Talsha1995@users.noreply.github.com>
2020-05-14 16:46:11 +0300

    Update Match.c
    comments

--- source/Match.c
+++ source/Match.c
@@ -7,39 +7,39 @@
 
 Match *MatchCreate(Team *teamH, Team *teamG, int goalH, int goalG)
 {
-    Match *match;
-    match = (Match *) malloc(sizeof(Match));
-    if (match == NULL)
+    Match *match; //pointer for Match
+    match = (Match *) malloc(sizeof(Match)); //allocate memory for single match
+    if (match == NULL) // check if malloc worked
     {
         printf("Failed to allocate %lu bytes\n", sizeof(Match));
         exit(1);
     }
-    match->teamHost = teamH;
+    match->teamHost = teamH; 
     match->teamGuest = teamG;
     match->goalGuest = goalG;
     match->goalHost = goalH;
-    return match;
+    return match; // return new match after build it
 
 }
 
 void MatchDestroy(Match *match)
 {
-    TeamDestroy(match->teamGuest);//destroy guest of match
+    TeamDestroy(match->teamGuest);//destroy guest of match 
     TeamDestroy(match->teamHost);//destroy host of match
     free(match);//destroy match itself
 }
 
-bool team_participated(const Match *match, const Team *team)
+bool team_participated(const Match *match, const Team *team) // chek if team participated in specific match
 {
     return ((team->TeamName == match->teamGuest->TeamName) || (team->TeamName == match->teamHost->TeamName));
 }
 
-bool match_tied(const Match *match)
+bool match_tied(const Match *match) // check tie
 {
     return match->goalGuest == match->goalHost;
 }
 
-bool team_won(const Match *match, const Team *team)
+bool team_won(const Match *match, const Team *team) // check if team won in match
 {
     if (!team_participated(match, team))
     {
@@ -58,7 +58,7 @@ bool team_won(const Match *match, const Team *team)
     return match->teamHost->TeamName == team->TeamName;
 }
 
-bool team_lost(const Match *match, const Team *team)
+bool team_lost(const Match *match, const Team *team) check if team lost in match
 {
     if (!team_participated(match, team))
     {
@@ -70,7 +70,7 @@ bool team_lost(const Match *match, const Team *team)
 
 }
 
-int GF(const Match *match, const Team *team)
+int GF(const Match *match, const Team *team) // return how many goals for the team in match
 {
     if (!team_participated(match, team))
     {
@@ -82,7 +82,7 @@ int GF(const Match *match, const Team *team)
 
 }
 
-int GA(const Match *match, const Team *team)
+int GA(const Match *match, const Team *team) // return how many goals against the team in match
 {
     if (!team_participated(match, team))
     {
